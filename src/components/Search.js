ac8ff74995e1f80d53fc25710e33e0fcc5c87847
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2021-05-10 23:23:01 +0530

    expand

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,6 +8,7 @@ const Search = () => {
 
     const [activeBtn, setActiveBtn] = useState('liftNo');
     const [searchKey, setSearchKey] = useState('');
+    const [expandId, setExpandId] = useState(-1);
 
     const [result, setResult] = useState(data);
 
@@ -16,24 +17,76 @@ const Search = () => {
     let size = Object.keys(result).length
 
     const results = new Array();
-
+    const expand = { minHeight: "40rem" }
+    const compress = { minHeight: "6rem" }
+    let style;
     for (let i = 0; i < size; i++) {
-        results.push(<div className='search-result'>
-            <span>
-                <h1 className='q'>Date :</h1>
-                <h1 className='result-date' type="date" >{result[i].date}</h1>
-            </span>
-            <span>
-                <h1 className='q'>Lift No :</h1>
-                <h1 className='result-lift-no' type="text" >{result[i].liftNo}</h1>
-            </span>
-            <span>
-                <h1 className='q'>Name :</h1>
-                <h1 className='result-lift-customer' type="text" >{result[i].customerName}</h1>
-            </span>
-        </div>
+        style = compress;
+        if (i == expandId) {
+            style = expand;
+        }
+        results.push(
+            <div className='search-result' style={style} id={i} >
+                <span>
+                    <h1 className='q'>Date :</h1>
+                    <h1 className='result-date' type="date" >{result[i].date}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Lift No :</h1>
+                    <h1 className='result-lift-no' type="text" >{result[i].liftNo}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Name :</h1>
+                    <h1 className='result-lift-customer' type="text" >{result[i].customerName}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Mod :</h1>
+                    <h1 className='result-date' type="date" >{result[i].mod}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Pod :</h1>
+                    <h1 className='result-lift-no' type="text" >{result[i].pod}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Breakdown :</h1>
+                    <h1 className='result-lift-customer' type="text" >{result[i].breakdown}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Service :</h1>
+                    <h1 className='result-date' type="date" >{result[i].service}</h1>
+                </span>
+
+                <span>
+                    <h1 className='q'>Site Name :</h1>
+                    <h1 className='result-lift-customer' type="text" >{result[i].siteName}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Mechanic Name :</h1>
+                    <h1 className='result-lift-customer' type="text" >{result[i].mechanicName}</h1>
+                </span>
+                <span>
+                    <h1 className='q'>Remark :</h1>
+                    <h1 className='result-date' type="date" >{result[i].remark}</h1>
+                </span>
+                <button name={i} className='expand-btn' onClick={triggerExpand} ><img name={i} src="images/icons/expand.png" alt="" /></button>
+            </div>
+
         )
     }
+
+    function triggerExpand(e) {
+
+        if (e.target.name != expandId) {
+            console.log(e.target.name);
+            setExpandId(e.target.name);
+        }
+        else {
+            setExpandId(-1);
+        }
+
+
+    }
+
 
 
     function buttonProvider() {
